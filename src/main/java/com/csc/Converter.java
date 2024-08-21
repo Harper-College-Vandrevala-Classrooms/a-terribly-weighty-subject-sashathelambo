be98329f7445f9ad5b95d642795aea10a94a7e43
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-08-21 13:19:01 -0500

    Update Converter.java

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -2,25 +2,35 @@ package com.csc;
 
 public class Converter {
 
-  
-  public String toPounds(int ounces) {
-    double pounds = ounces / 16.0; 
-    String result = String.format("%.4f", pounds);
-    if (pounds == 1.0) {
-      return result + " lb"; 
-    } else {
-      return result + " lbs"; 
+    public String toPounds(int ounces) {
+        double pounds = ounces / 16.0; 
+        String result = String.format("%.4f", pounds);
+        if (pounds == 1.0) {
+            return result + " lb"; 
+        } else {
+            return result + " lbs"; 
+        }
     }
-  }
 
- 
-  public String toPoundsAndOunces(int ounces) {
-    int pounds = ounces / 16;
-    int remainingOunces = ounces % 16;
-    if (pounds == 1) {
-      return pounds + " lb " + remainingOunces + " oz";
-    } else {
-      return pounds + " lbs " + remainingOunces + " oz";
+    public String toPoundsAndOunces(int ounces) {
+        int pounds = ounces / 16;
+        int remainingOunces = ounces % 16;
+        if (pounds == 1) {
+            return pounds + " lb " + remainingOunces + " oz";
+        } else {
+            return pounds + " lbs " + remainingOunces + " oz";
+        }
     }
-  }
+
+    public static void main(String[] args) {
+        Converter converter = new Converter();
+
+        // Test the toPounds method
+        int ounces1 = 32;
+        System.out.println(ounces1 + " ounces is " + converter.toPounds(ounces1));
+
+        // Test the toPoundsAndOunces method
+        int ounces2 = 45;
+        System.out.println(ounces2 + " ounces is " + converter.toPoundsAndOunces(ounces2));
+    }
 }
